1a3402d094a55a8626b31b2bb52c4470d587641b
Sachin Ayeshmantha <132859045+Sachin20010517@users.noreply.github.com>
2024-03-25 09:51:37 +0530

    Just corrected Sudarshan's mistakes

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,35 +1,38 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Define the User schema with new fields
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+    },
+    longitude: {
+      type: String,
+      required: true,
+    },
+    latitude: {
+      type: String,
+      required: true,
+    },
   },
-  password: { 
-    type: String,
-    required: true
-  },
-  address: {
-    type: String,
-    required: true
-  },
-  mobile: {
-    type: String,
-    required: true
-  },
-  longitude: {
-    type: String,
-    required: true 
-  },
-  latitude: {
-    type: String,
-    required: true
-  },
-}, { timestamps: true }); // Add timestamps to automatically add createdAt and updatedAt
+  { timestamps: true }
+); // Add timestamps to automatically add createdAt and updatedAt
 
 // Create the User model
-const Users = mongoose.model('User', userSchema, 'users');
+const Users = mongoose.model("User", userSchema, "users");
 
 module.exports = Users;
